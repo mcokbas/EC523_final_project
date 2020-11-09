dfbe4f936fec03356b68ebf311c7e6c9bda25dca
MinxuPeng <mxpeng@bu.edu>
2020-11-09 18:01:36 -0500

    Confirmed and changed the noise model to fit the TR method.
    The corresponding  Poisson_Poisson_Noise_Example notebook is also updated.

--- BM3D/noise_model.py
+++ BM3D/noise_model.py
@@ -1,29 +1,65 @@
 import numpy as np
 
 def poissonpoissonnoise(X, min_eta, max_eta , Lambda, t):
-  #X: Input matrix containing grayscale values as double [0,1]
-  #min_eta: Minimum eta after scaling and shifting 
-  #max_eta: Maximum eta after scaling and shifting
-  #Lambda: Poisson process rate per unit time
-  #t: Dwell time
+    """
+    Generate observation y (total SE counts) which obeys compound Poisson-Poisson distribution
+    with total dose Lambda*t. The ground truth image is a 2-D array, scaled to [min_eta, max_eta]
 
-  #Get Dimensions of Image
-  [d1,d2] = X.shape 
-  #Vectorize Image
-  X = np.reshape(X,[d1*d2,1])
-  #Means for # of SE's (M)
-  eta = (max_eta - min_eta)*X + min_eta
-  M = np.random.poisson( lam = Lambda*t, size= (d1*d2,1))
-  #Initialize noisy image
-  y = np.zeros((d1*d2,1))
+    Args:
+        X: ground truth image, i.e. mean SE yield.
+        min_eta: minimum eta value after scaling and shifting.
+        max_eta: maximum eta value after scaling and shifting.
+        Lambda: dose rate.
+        t: dwell time.
 
-  #Loop on each pixel for calculation of noisy image
-  for i in range(d1*d2):
-    #Assign noisy pixel
-    y[i] = np.sum( np.random.poisson( lam = eta[i], size= M[i] ) )
+    Returns:
+        y: the total SE counts as an image. It's scaled in [0, 1].
+        y_tr: a stack of t number of low-dose SE count images. It haven't been scaled yet.
 
-  #Rescale image so that it is \in [0,1]
-  y = y / np.max(y)
-  #Reshape for image
-  y = np.reshape( y,[d1,d2,1] )
-  return y
+    Shapes:
+        input:
+            X: [d1, d2], d1 and d2 are height and width of the image.
+            min_eta: scalar.
+            max_eta: scalar.
+            Lambda: scalar
+            t: scalar.
+        output:
+            y: [d1, d2]
+            y_tr: [d1, d2, t]
+
+    """
+    #X: Input matrix containing grayscale values as double [0,1]
+    #min_eta: Minimum eta after scaling and shifting
+    #max_eta: Maximum eta after scaling and shifting
+    #Lambda: Poisson process rate per unit time
+    #t: Dwell time
+
+    # Get Dimensions of Image
+    [d1, d2] = X.shape
+    # Vectorize Image
+    X = np.reshape(X, [d1*d2, 1])
+
+    #Means for # of SE's (M)
+    eta = (max_eta - min_eta)*X + min_eta
+
+    # Generate time-resolved ions
+    ions = np.random.poisson(lam=Lambda, size=(X.size, t))
+
+    # M is the total ions.
+    M = np.sum(ions, axis=1)
+
+    # Create time-resolved observations y_tr
+    y_tr = np.random.poisson(eta * ions)
+
+    # y is the total SE counts.
+    y = np.sum(y_tr, axis=1)
+
+    #Rescale image so that it is \in [0,1]
+    y = y / np.max(y)
+
+    #Reshape for image
+    y_tr = y_tr.reshape([d1, d2, t])
+    y = np.reshape(y, [d1, d2])
+
+    return y, y_tr
+
