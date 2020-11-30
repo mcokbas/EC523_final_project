00b9530bcf048629a099589253ca90f32380d727
MinxuPeng <mxpeng@bu.edu>
2020-11-29 21:42:02 -0500

    Update the noise_model.py.
    Now it can handle 3d image arrays.

--- BM3D/noise_model.py
+++ BM3D/noise_model.py
@@ -1,6 +1,7 @@
 import numpy as np
+import torch
 
-def poissonpoissonnoise(X, min_eta, max_eta , Lambda, t):
+def poissonpoissonnoise(X, min_eta, max_eta, Lambda, t):
     """
     Generate observation y (total SE counts) which obeys compound Poisson-Poisson distribution
     with total dose Lambda*t. The ground truth image is a 2-D array, scaled to [min_eta, max_eta]
@@ -9,7 +10,7 @@ def poissonpoissonnoise(X, min_eta, max_eta , Lambda, t):
         X: ground truth image, i.e. mean SE yield.
         min_eta: minimum eta value after scaling and shifting.
         max_eta: maximum eta value after scaling and shifting.
-        Lambda: dose rate.
+        Lambda: dose rate. Dose for each sub-acquisition.
         t: dwell time.
 
     Returns:
@@ -19,6 +20,7 @@ def poissonpoissonnoise(X, min_eta, max_eta , Lambda, t):
     Shapes:
         input:
             X: [d1, d2], d1 and d2 are height and width of the image.
+               or [c_chanel, d1, d2], where c_chanel is the number of color channel.
             min_eta: scalar.
             max_eta: scalar.
             Lambda: scalar
@@ -34,32 +36,43 @@ def poissonpoissonnoise(X, min_eta, max_eta , Lambda, t):
     #Lambda: Poisson process rate per unit time
     #t: Dwell time
 
-    # Get Dimensions of Image
-    [d1, d2] = X.shape
-    # Vectorize Image
-    X = np.reshape(X, [d1*d2, 1])
+    if isinstance(X, torch.Tensor):
+        X = X.cpu().detach().numpy()
 
-    #Means for # of SE's (M)
-    eta = (max_eta - min_eta)*X + min_eta
+    if X.ndim == 2:
+        # Get Dimensions of Image
+        [d1, d2] = X.shape
+        # Vectorize Image
+        X = np.reshape(X, (d1*d2, 1))
+    elif X.ndim == 3:
+        [color_cn, d1, d2] = X.shape
+        X = np.reshape(X, (color_cn, d1 * d2, 1))
+
+    # Rescale X to be in [max_eta, min_eta]
+    # slope = (max_eta - min_eta) / (X.max() - X.min())
+    # distance = min_eta - X.min() * slope
+    # eta = slope * X + distance
+    eta = (max_eta - min_eta) * X + min_eta
 
     # Generate time-resolved ions
-    ions = np.random.poisson(lam=Lambda, size=(X.size, t))
+    ions = np.random.poisson(lam=Lambda, size=(*X.shape[:-1], t))
 
-    # M is the total ions.
-    M = np.sum(ions, axis=1)
+    # # M is the total ions.
+    # M = np.sum(ions, axis=1)
 
     # Create time-resolved observations y_tr
     y_tr = np.random.poisson(eta * ions)
 
     # y is the total SE counts.
-    y = np.sum(y_tr, axis=1)
+    y = np.sum(y_tr, axis=-1)
 
-    #Rescale image so that it is \in [0,1]
+    # Rescale image so that it is \in [0,1]
     y = y / np.max(y)
+    y = y.astype("float32")
 
-    #Reshape for image
-    y_tr = y_tr.reshape([d1, d2, t])
-    y = np.reshape(y, [d1, d2])
+    # Reshape for image
+    y_tr = np.reshape(y_tr, newshape=(*X.shape[:-2], d1, d2, t))
+    y = np.reshape(y, newshape=(*X.shape[:-2], d1, d2))
 
     return y, y_tr
 
